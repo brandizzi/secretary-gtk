af72092dadabb1fcb27ca226799ba49643d262ff
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2012-06-15 09:25:15 -0300

    no more freeing project name

--- project-tree-model.c
+++ project-tree-model.c
@@ -7,12 +7,10 @@ GtkWidget *sct_gtk_project_tree_model_new(Secretary *secretary) {
 
     for (int i = 0; i < n_projects; i++) {
         Project *project = secretary_get_nth_project(secretary, i);
-        const char *name = project_get_name(project);
         GtkTreeIter iter;
         gtk_tree_store_append(model, &iter, NULL);
         gtk_tree_store_set(model, &iter, SCT_GTK_PROJECT_PERSPECTIVE_COLUMN,
-                name, -1);
-        free(name);
+                project_get_name(project), -1);
     }
     return model;
 }
--- task-tree-model.c
+++ task-tree-model.c
@@ -6,9 +6,9 @@ GtkListStore *sct_gtk_task_tree_model_new(SctGtkApplication *app) {
     GtkListStore *model = gtk_list_store_new(SCT_GTK_TASK_COLUMN_COUNT,
             G_TYPE_BOOLEAN, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
     Secretary *secretary = notebook_get_secretary(app->notebook);
-    int n_projects = secretary_count_projects(secretary);
+    int n_tasks = secretary_count_tasks(secretary, false)+secretary_count_tasks(secretary, true);
 
-    for (int i = 0; i < n_projects; i++) {
+    for (int i = 0; i < n_tasks; i++) {
         Task *task = secretary_get_nth_task(secretary, i);
         GtkTreeIter iter;
         gtk_list_store_append(model, &iter);
@@ -19,16 +19,10 @@ GtkListStore *sct_gtk_task_tree_model_new(SctGtkApplication *app) {
         gtk_list_store_set(model, &iter, SCT_GTK_TASK_DESCRIPTION_COLUMN,
                 description, -1);
         free(description);
-        
-        if (task_has_project(task)) {
-            Project *project = task_get_project(task);
-            const char *project_name = project_get_name(project);
-            gtk_list_store_set(model, &iter, SCT_GTK_TASK_PROJECT_COLUMN,
-                    project_name, -1);
-        } else {
-            gtk_list_store_set(model, &iter, SCT_GTK_TASK_PROJECT_COLUMN,
-                    NULL, -1);
-        }
+    
+        Project *project = task_get_project(task);
+        gtk_list_store_set(model, &iter, SCT_GTK_TASK_PROJECT_COLUMN,
+                project? project_get_name(project) : "" , -1);
         char buffer[DATE_SIZE] = "";
         if (task_is_scheduled(task)) {
             time_t secs = task_get_scheduled_date(task);
