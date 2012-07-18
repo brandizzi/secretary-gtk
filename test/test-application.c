606b3e5f5d46074e532d8631a677ee395be45fe7
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2012-07-18 12:51:49 -0300

    Assuring inbox is selected at start

--- test/test-application.c
+++ test/test-application.c
@@ -34,6 +34,35 @@ static void test_sct_gtk_app_select_inbox(CuTest *test) {
     sct_gtk_application_free(app);
 }
 
+static void test_sct_gtk_app_select_inbox_at_start(CuTest *test) {
+    remove("tempfile");
+    Notebook *notebook = notebook_new("tempfile");
+    Secretary *secretary = notebook_get_secretary(notebook);
+    
+    Project *project;
+    Task *task_in_inbox, *task_in_project, *task_scheduled, 
+            *task_scheduled_future;
+    test_sct_gtk_util_create_project_and_four_tasks(secretary, &project, 
+            &task_in_inbox, &task_scheduled, &task_scheduled_future,
+            &task_in_project);    
+            
+    SctGtkApplication *app = sct_gtk_application_new(notebook);
+    
+        // Selecting inbox tasks
+    sct_gtk_application_select_path_on_project_treeview(app, 
+            SCT_GTK_PROJECT_PATH_INBOX);
+
+    gchar *name = 
+        test_sct_gtk_util_get_task_description_from_list_view(app, 0);
+    
+    CuAssertStrEquals(test, "Inbox task", name);
+    
+    name = test_sct_gtk_util_get_task_description_from_list_view(app, 1);
+    CuAssertStrEquals(test, NULL, name);
+    
+    sct_gtk_application_free(app);
+}
+
 static void test_sct_gtk_app_select_scheduled(CuTest *test) {
     remove("tempfile");
     Notebook *notebook = notebook_new("tempfile");
@@ -155,13 +184,15 @@ static void test_sct_gtk_app_select_project_should_not_crash(CuTest *test) {
     sct_gtk_application_free(app);
 }
 
+
 CuSuite *test_sct_gtk_app_suite(void) {
     CuSuite *suite  = CuSuiteNew();
-    SUITE_ADD_TEST(suite, test_sct_gtk_app_select_project_should_not_crash);
     SUITE_ADD_TEST(suite, test_sct_gtk_app_select_inbox);
+    SUITE_ADD_TEST(suite, test_sct_gtk_app_select_inbox_at_start);
     SUITE_ADD_TEST(suite, test_sct_gtk_app_select_scheduled);
     SUITE_ADD_TEST(suite, test_sct_gtk_app_select_scheduled_for_today);
     SUITE_ADD_TEST(suite, test_sct_gtk_app_select_project_child);
+    SUITE_ADD_TEST(suite, test_sct_gtk_app_select_project_should_not_crash);
 }
 
 
