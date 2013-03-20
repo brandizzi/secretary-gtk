f56a5ea7b1f1f71ad9d2d62a4be81a61b2bb0060
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2013-03-19 23:08:39 -0300

    Better dimensions

--- panel.c
+++ panel.c
@@ -7,10 +7,16 @@
 
 
 GtkWidget *sct_gtk_panel_new(SctGtkApplication *app) {
-    GtkWidget *paned = gtk_hpaned_new();
-    
-    gtk_paned_add1(GTK_PANED(paned), sct_gtk_perspective_tree_view_new(app));
-    gtk_paned_add2(GTK_PANED(paned), sct_gtk_task_tree_view_new(app));
+    GtkWidget *paned = gtk_hpaned_new(),
+              *perspective_tree_view = sct_gtk_perspective_tree_view_new(app),
+              *task_tree_view = sct_gtk_task_tree_view_new(app);
+
+    gtk_widget_set_size_request(perspective_tree_view, 300, 600);
+    gtk_widget_set_size_request(task_tree_view, 600, 600);
+
+    gtk_paned_add1(GTK_PANED(paned), perspective_tree_view);
+    gtk_paned_add2(GTK_PANED(paned), task_tree_view);
+
 
     gtk_widget_show(paned);
     
